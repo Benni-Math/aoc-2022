f9a340d5a7d9605540ba787534f362847ec3f676
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-12-15 20:17:43 -0500

    day1.1 in Rust

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{ BufReader, BufRead };
+
+
+const INPUT_FILENAME: &str = "../../inputs/day01-input.txt";
+
+struct SumMax {
+    max: u64,
+    sum: u64,
+}
+
+fn main() -> std::io::Result<()> {
+    let file = File::open(INPUT_FILENAME)?;
+    let reader = BufReader::new(file);
+
+    let answer = reader
+        .lines()
+        .flatten()
+        .fold(
+            SumMax { max: 0, sum: 0 },
+            |sum_max, line| {
+                let SumMax { max: curr_max, sum: curr_sum } = sum_max;
+                match line.as_str() {
+                    "" => SumMax {
+                        max: if curr_sum > curr_max { curr_sum } else { curr_max },
+                        sum: 0,
+
+                    },
+                    s => SumMax {
+                        max: curr_max,
+                        sum: curr_sum + s.parse::<u64>().unwrap()
+                    } 
+                }
+            }
+        )
+        .max;
+
+    println!("Answer: {}", answer);
+
+    
+    Ok(())
 }
