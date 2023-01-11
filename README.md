f287f127d274beb787fd361e6cc511d7eac3be72
Benni-Math <benediktjens.arnarsson@gmail.com>
2023-01-11 14:37:37 -0500

    Added pre-compilation step

--- README.md
+++ README.md
@@ -2,7 +2,73 @@
 
 I'll be trying to solve some of the AOC 2022 problems.
 
-The idea is to do them in Python, Typescript, and Rust,
-but, honestly, it might not pan out like that.
+To run, get you're AoC 2022 inputs and place them in a directory name `inputs/` in the root of this project. The inputs must be named '01.txt', '02.txt', and so on until '25.txt'. Then, just run `python run_all.py` in the command line and the outputs for each programming language will be created in the `outputs/` directory.
 
-Future plans: C++, Elixir, Haskell, Julia
+## Requirements
+
+To run everything, you'll need the following:
+
+ - Python 3.10 (or higher)
+    - 3.10 needed for `match-case` statements
+ - Typescript 4.9.4^ & Node v16 (ES2022)
+    - ES2022 support needed for `String.prototype.replaceAll()` 
+ - Rust/Cargo 1.66
+ - Elixir/Mix 1.14.2 and Erlang/OTP 25 
+
+## Future Languages
+
+ - Gleam
+ - C/C++
+ - Forth
+ - Common Lisp
+ - Smalltalk
+ - OCaml
+ - Julia
+ - Haskell
+ - Ruby
+ - Clojure
+ - Kotlin/Java
+ - C#
+ - F#
+ - PHP
+ - Go
+ - Swift
+ - Val
+ - Zig
+ - Odin
+ - Vale
+ - Pony
+ - Wren
+ - Perl/Raku
+ - SNOBOL (SPITBOL)
+ - Lua
+ - V 
+ - D 
+ - Idris
+ - Agda
+ - Lean
+ - Coq
+ - ARM64 assembly
+ - J
+ - Ada 
+ - Dart
+ - Nim
+ - Erlang
+ - Lobster
+ - Scheme
+ - Io
+ - Prolog
+ - Factor
+ - Elm
+ - PureScript
+ - MiniKanren
+ - Curry
+ - Alice
+ - Tcl
+ - AWK
+ - Fjolnir
+
+or almost anything from 
+[this list](https://github.com/robertmuth/awesome-low-level-programming-languages)
+or 
+[this list](https://learnxinyminutes.com).
--- run_all.py
+++ run_all.py
@@ -11,51 +11,89 @@ import os
 import time
 from io import TextIOWrapper
 
-def run_one(cmd: list[str], cwd: str, err_file: TextIOWrapper):
-    """Runs a single AOC directory"""
+OUTPUT_DIR = 'outputs'
+INPUT_DIR = 'inputs'
+
+def _run_one(cmd: list[str], cwd: str, err_file: TextIOWrapper):
+    """Runs a single AOC directory."""
     lang_name = cwd.capitalize()
     print(f'Running {lang_name} code...')
     start = time.perf_counter()
-    output_filename = f'outputs/{cwd}.txt'
+    output_filename = f'{OUTPUT_DIR}/{cwd}.txt'
     with open(output_filename, 'w', encoding='utf-8') as out_file:
         subprocess.Popen(
             cmd,
             cwd=cwd,
             stdout=out_file,
             stderr=err_file,
+        ).wait()
+    print(f'{lang_name} took {time.perf_counter()-start:.4g} seconds\n')
+
+
+def compile_all():
+    """Compile the projects that need compiling."""
+
+    my_env = os.environ.copy()
+    my_env['MIX_ENV'] = 'release'
+    print('Compiling...')
+    compile_filename = f'{OUTPUT_DIR}/compile.txt'
+    with open(compile_filename, 'w', encoding='utf-8') as out_file:
+        rs_c = subprocess.Popen(
+            ['cargo', 'build', '-r'],
+            cwd='rust',
+            # stdout=subprocess.DEVNULL,
+            stdout=out_file,
+            stderr=subprocess.STDOUT,
         )
-    print(f'{lang_name} took {time.perf_counter()-start} seconds\n')
+        ex_c = subprocess.Popen(
+            ['mix', 'escript.build'],
+            cwd='elixir',
+            # stdout=subprocess.DEVNULL,
+            stdout=out_file,
+            stderr=subprocess.STDOUT,
+            env=my_env,
+        )
+        ts_c = subprocess.Popen(
+            ['npm', 'run', 'build'],
+            cwd='typescript',
+            # stdout=subprocess.DEVNULL,
+            stdout=out_file,
+            stderr=subprocess.STDOUT,
+        )
+        _waiting = [p.wait() for p in (rs_c, ex_c, ts_c)]
+    print('Finished compiling.\n')
 
 
 def run_all():
-    """Runs all of the AOC code"""
+    """Runs all of the AOC code."""
+    if not os.path.exists(INPUT_DIR):
+        raise Exception('Create an input directory.')
 
     # Create output directory
-    if not os.path.exists('outputs'):
-        os.makedirs('outputs')
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
     # Error file
-    err_file = open('outputs/errors.txt', 'w', encoding='utf-8')
+    with  open(f'{OUTPUT_DIR}/errors.txt', 'w', encoding='utf-8') as err_file:
 
-    py_cwd = 'python'
-    py_cmd = ['python', 'main.py']
-    run_one(py_cmd, py_cwd, err_file)
+        ex_cwd = 'elixir'
+        ex_cmd = ['target/aoc']
+        _run_one(ex_cmd, ex_cwd, err_file)
 
-    ts_cwd = 'typescript'
-    ts_cmd = ['npm', 'run', 'start']
-    run_one(ts_cmd, ts_cwd, err_file)
+        py_cwd = 'python'
+        py_cmd = ['python', 'main.py']
+        _run_one(py_cmd, py_cwd, err_file)
 
-    rs_cwd = 'rust'
-    rs_cmd = ['cargo', 'run', '-r']
-    run_one(rs_cmd, rs_cwd, err_file)
+        ts_cwd = 'typescript'
+        ts_cmd = ['node', 'target/aoc']
+        _run_one(ts_cmd, ts_cwd, err_file)
 
-    ex_cwd = 'elixir'
-    ex_cmd = ['mix', 'run', 'aoc.exs']
-    run_one(ex_cmd, ex_cwd, err_file)
-
-    err_file.close()
+        rs_cwd = 'rust'
+        rs_cmd = ['target/release/aoc-2022']
+        _run_one(rs_cmd, rs_cwd, err_file)
 
     print('Check the outputs/ directory for the results!')
 
 if __name__ == '__main__':
+    compile_all()
     run_all()
